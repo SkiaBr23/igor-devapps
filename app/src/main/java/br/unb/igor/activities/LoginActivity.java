6cd09fe2f67c169e6dc1af74d69687e92cb7b1c8
Maximillian Xavier <maximillianfx@gmail.com>
2017-08-22 09:29:42 -0300

    Animação splash screen

--- app/src/main/java/br/unb/igor/activities/LoginActivity.java
+++ app/src/main/java/br/unb/igor/activities/LoginActivity.java
@@ -1,11 +1,12 @@
 package br.unb.igor.activities;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.unb.igor.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/br/unb/igor/activities/SplashScreen.java
+++ app/src/main/java/br/unb/igor/activities/SplashScreen.java
@@ -4,25 +4,44 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
+import android.text.LoginFilter;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import br.unb.igor.R;
 
 public class SplashScreen extends Activity {
+
+    private ImageView logoAppSplashScreen;
+    private static int SPLASH_SCREEN_TIMEOUT = 3000;
+    private static int ANIMATION_LOGO = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        int SPLASH_SCREEN_TIMEOUT = 3000;
+        logoAppSplashScreen = (ImageView)findViewById(R.id.logoAppSplashScreen);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent maintIntent = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(maintIntent);
-                overridePendingTransition(R.anim.fade_in_one,R.anim.fade_out);
-                finish();
+                Animation anim = new TranslateAnimation(Animation.ABSOLUTE,Animation.ABSOLUTE,Animation.ABSOLUTE,-600);
+                anim.setDuration(2000);
+                anim.setFillAfter(true);
+                logoAppSplashScreen.startAnimation(anim);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent (SplashScreen.this, LoginActivity.class);
+                        startActivity(mainIntent);
+                        overridePendingTransition(R.anim.fade_in_one,R.anim.fade_out);
+                        finish();
+                    }
+                }, SPLASH_SCREEN_TIMEOUT);
             }
-        }, SPLASH_SCREEN_TIMEOUT);
+        }, ANIMATION_LOGO);
     }
 }
