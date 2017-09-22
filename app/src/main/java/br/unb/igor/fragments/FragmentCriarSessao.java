f3e4213491fd98f20a2ffbd56dc87900d5ee5ce7
Maximillian Xavier <maximillianfx@gmail.com>
2017-09-22 11:20:31 -0300

    Inclusao de datepicker na criação da sessão

--- app/src/main/java/br/unb/igor/fragments/FragmentCriarSessao.java
+++ app/src/main/java/br/unb/igor/fragments/FragmentCriarSessao.java
@@ -1,6 +1,7 @@
 package br.unb.igor.fragments;
 
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,8 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import br.unb.igor.R;
 import br.unb.igor.helpers.SessionListener;
@@ -29,6 +35,9 @@ public class FragmentCriarSessao extends Fragment {
     private Button btnConfirmarSessao;
     private String keyAdventure;
     private EditText editTituloSessao;
+    private Calendar myCalendar;
+    private Button btnDataSessao;
+    private  DatePickerDialog.OnDateSetListener date;
 
     private SessionListener mListener;
 
@@ -64,6 +73,7 @@ public class FragmentCriarSessao extends Fragment {
         btnCloseSessao = (ImageView)root.findViewById(R.id.btnCloseSessao);
         btnConfirmarSessao = (Button)root.findViewById(R.id.btnConfirmarSessao);
         editTituloSessao = (EditText)root.findViewById(R.id.editTituloSessao);
+        btnDataSessao = (Button)root.findViewById(R.id.btnDataSessao);
 
 
         keyAdventure = getArguments().getString("keyAventura");
@@ -86,9 +96,10 @@ public class FragmentCriarSessao extends Fragment {
         btnConfirmarSessao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (editTituloSessao.getText().toString().length() == 0) {
+                if ((editTituloSessao.getText().toString().length() == 0) || btnDataSessao.getText().equals("00/00/00")) {
                     editTituloSessao.setError("Preencha com o título!");
                     editTituloSessao.setTextColor(Color.BLACK);
+                    btnDataSessao.setError("Seleciona uma data!");
                 } else {
                     View view2 = getActivity().getCurrentFocus();
                     //Fecha o keyboard, ao final da criação da aventura
@@ -103,7 +114,35 @@ public class FragmentCriarSessao extends Fragment {
             }
         });
 
+
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        btnDataSessao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         return root;
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+        btnDataSessao.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
