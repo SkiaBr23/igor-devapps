c775b05372974023f5368f1a2e645fde6f12dd9d
maximillianfx <maximillianfx@gmail.com>
2017-11-17 15:09:42 -0200

    Layout e model Livro para adição de livros na base do app

---
+++ app/src/main/java/br/unb/igor/model/Livro.java
@@ -0,0 +1,55 @@
+package br.unb.igor.model;
+
+import android.graphics.Bitmap;
+
+public class Livro {
+
+    String titulo;
+    String idAddedBy;
+    String urlFile;
+    Bitmap capaLivro;
+    boolean statusDownload;
+
+    public Livro() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdAddedBy() {
+        return idAddedBy;
+    }
+
+    public void setIdAddedBy(String idAddedBy) {
+        this.idAddedBy = idAddedBy;
+    }
+
+    public String getUrlFile() {
+        return urlFile;
+    }
+
+    public void setUrlFile(String urlFile) {
+        this.urlFile = urlFile;
+    }
+
+    public Bitmap getCapaLivro() {
+        return capaLivro;
+    }
+
+    public void setCapaLivro(Bitmap capaLivro) {
+        this.capaLivro = capaLivro;
+    }
+
+    public boolean isDownloaded() {
+        return statusDownload;
+    }
+
+    public void setDownloaded(boolean statusDownload) {
+        this.statusDownload = statusDownload;
+    }
+}
